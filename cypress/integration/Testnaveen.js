7244981597a00b81563264527197309923b830aa
Apurv Patel <apurvpatel@APs-MacBook-Pro.local>
2021-08-29 13:06:53 -0400

    first committ

---
+++ cypress/integration/Testnaveen.js
@@ -0,0 +1,18 @@
+/// <reference types = 'cypress' />
+
+describe('Test practice' , function()
+{
+    it('Test Case ',() =>{
+
+        cy.visit('http://automationpractice.com/index.php')
+
+        cy.get('ul#homefeatured>li:nth-of-type(5)').click();
+        cy.get('span.cross').click({ force: true });
+        cy.wait(3000);
+
+        cy.contains('automationpractice').trigger('mouseover');
+        
+    })
+
+
+})
